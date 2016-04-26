bf4bf47e1a708070a33780eba1df7b1c69c5b669
Rickie Kerndt <kerndtr@kerndt.com>
2016-04-25 22:42:25 -0700

    add debug message for field to check for duplciates

--- greedy_neighbor.py
+++ greedy_neighbor.py
@@ -217,7 +217,7 @@ def on_neighbor(client, userdata, msg):
 def on_field(client, userdata, msg):
 
     msg_type, value = parse_msg(msg)
-
+    print("Received msg id={}, type={}, value={}".format(msg.id, msg_type, value))
     if msg_type == Msg.enter_field:
         userdata.role.occupants.add(value)
         print("Neighbor {} has entered the field".format(value))
